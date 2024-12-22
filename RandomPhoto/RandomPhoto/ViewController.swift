92aa09c4e38472da8674027f6a11451fadf7cac8
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2024-12-22 19:22:17 +0000

    test background color

---
+++ RandomPhoto/RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Maryna on 22/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
---
+++ RandomPhoto/RandomPhotoTests/RandomPhotoTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoTests.swift
+//  RandomPhotoTests
+//
+//  Created by Maryna on 22/12/2024.
+//
+
+import Testing
+@testable import RandomPhoto
+
+struct RandomPhotoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
