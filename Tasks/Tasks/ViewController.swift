90fa8f3177c84b0aa99780e46d8232d022cc9064
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2024-12-24 20:33:09 +0000

    Add: Initial setup for Tasks app

---
+++ Tasks/Tasks/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Maryna on 24/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+}
