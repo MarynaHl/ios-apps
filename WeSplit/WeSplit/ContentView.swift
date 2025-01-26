1c138e9946666edfd1b90b82210a7184e3f9c97b
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-01-26 15:31:47 +0000

    Add TextField with @State and dynamic Text binding

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -1,15 +1,18 @@
 import SwiftUI // Імпортуємо SwiftUI для роботи з інтерфейсом
 
 struct ContentView: View {
-    @State private var tapCount = 0 // Використовуємо @State для зберігання стану
+    @State private var name = "" // Створюємо властивість @State для збереження введеного тексту
 
     var body: some View {
-        Button("Tap Count: \(tapCount)") { // Кнопка відображає поточний tapCount
-            self.tapCount += 1 // Збільшуємо tapCount на 1 при натисканні
+        Form { // Створюємо форму
+            TextField("Enter your name", text: $name)
+            // TextField використовує $name для двостороннього зв’язування
+            Text("Your name is \(name)")
+            // Text лише читає значення name, тому $ не використовується
         }
     }
 }
 
 #Preview {
-    ContentView() // Відображаємо попередній перегляд у SwiftUI
+    ContentView() // Відображення попереднього перегляду у SwiftUI
 }
