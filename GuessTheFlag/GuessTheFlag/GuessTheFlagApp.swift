a96f098d847f338df057b798a1097bf57d335b26
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-02-01 20:15:08 +0000

    Init new project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Maryna on 01/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
