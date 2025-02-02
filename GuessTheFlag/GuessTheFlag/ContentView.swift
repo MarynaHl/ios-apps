c70b8d3e8cc8e50753034c609820c44c72b0fa18
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-02-02 12:54:40 +0000

    Add LinearGradient example

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -3,22 +3,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            // Перехід кольорів у VStack
-            VStack(spacing: 0) {
-                Color.red.frame(height: UIScreen.main.bounds.height / 2)
-                Color.blue.frame(height: UIScreen.main.bounds.height / 2)
-            }
-            .ignoresSafeArea()
+            // Лінійний градієнт
+            LinearGradient(
+                colors: [.white, .black],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea() // Фон заповнює весь екран
 
-            // Текст поверх фону
-            VStack {
-                Text("Color Transition Example")
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .padding()
-                    .background(Color.black.opacity(0.6))
-                    .cornerRadius(10)
-            }
+            Text("Linear Gradient Example")
+                .font(.largeTitle)
+                .foregroundColor(.white)
         }
     }
 }
