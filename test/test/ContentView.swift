a1181d453c706174b6779b8f9bf801e5945c9f94
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-02-06 20:12:20 +0000

    "Initial View using Struct"  swift Copy Edit

---
+++ test/test/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("SwiftUI використовує Struct")
+                .font(.title)
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
