1d1c8210728b0958832d8fec46ed5968cc895a35
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-02-06 20:17:16 +0000

    Views as Properties

--- test/test/ContentView.swift
+++ test/test/ContentView.swift
@@ -1,12 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    var title: some View {
+        Text("Заголовок")
+            .font(.largeTitle)
+            .padding()
+    }
+    
     var body: some View {
         VStack {
-            Text("Цей текст зелений")
-            Text("Цей теж")
+            title
+            Text("Основний контент")
         }
-        .foregroundColor(.green) // Застосовується до всіх елементів
     }
 }
 
