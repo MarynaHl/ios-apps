37cb2c66143f04c9464a152b906f619916ccd809
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-02-15 22:25:27 +0000

    Initialize BetterRest project with Core ML support

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Maryna on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
