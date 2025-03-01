30855c33005ecb01f80a9541325b5561634c2961
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-03-01 19:49:45 +0000

    Initialize WordScramble project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Maryna on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
